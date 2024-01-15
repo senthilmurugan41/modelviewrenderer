bb75678ee1e3a23852c30eef6e90b7100d51f33d
senthilmurugan <senthilmurugan4321@gmail.com>
2024-01-15 23:04:14 +0530

    initial commit

---
+++ src/main/java/com/modelrenderer/ModelrendererApplication.java
@@ -0,0 +1,13 @@
+package com.modelrenderer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ModelrendererApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ModelrendererApplication.class, args);
+	}
+
+}
